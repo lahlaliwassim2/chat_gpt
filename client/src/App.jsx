0f60b3698b624d0d26e6f4b97d6c547c3ef5df1c
lahlaliwassim2 <wlahlali34@gmail.com>
2023-01-30 14:55:02 +0100

    preparation de ma structure avec laquelle je vais travailler

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,10 +6,15 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-     <div className="p-8 test-red-500">
-      Lahlali Wassim
-     </div>
+    <div>
+      {/* header  */}
+      <div className="header"></div>
+
+       {/* body  */}
+      <div className="body"></div>
+
+       {/* input  */}
+       <div className="input"></div>
     </div>
   )
 }
