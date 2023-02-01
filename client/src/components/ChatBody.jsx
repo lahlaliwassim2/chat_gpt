94f4271a2091f722447b6562fe2f46e97d6011b5
lahlaliwassim2 <wlahlali34@gmail.com>
2023-02-01 21:45:45 +0100

    ajouter les deux partie de chat de l'utulisateur et l'ai avec leurs styles

--- client/src/components/ChatBody.jsx
+++ client/src/components/ChatBody.jsx
@@ -1,19 +1,21 @@
 import React from 'react'
 
 function ChatBody() {
+    const aiStyle = "bg-white bg-opacity-40 backdrop-blur-lg dropshadow-md mr-auto "
+
   return (
 <div className="flex flex-col gap-4">
 {/* client message */}
-<div>
-    <pre>
+<div className='border-[#999999] break-words border-2 rounded-xl py-3 self-end px-3 max-w-[80%]'>
+    <pre className='whitespaces-pre-wrap'>
         <span>Hy Chat GPT, Can YOU HELP ME</span>
     </pre>
 </div>
 
 
 {/* ai message */}
-<div>
-    <pre>
+<div className={`border-[#999999] break-words border-2 rounded-xl py-3 self-end px-3 max-w-[80%] ${aiStyle}`}>
+    <pre className='whitespaces-pre-wrap'>
         <span>Yeah , I can help you with anything</span>
     </pre>
 </div>
