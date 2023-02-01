799e5360fb43e0f5076efec2f5f8ca9c5e7dac96
lahlaliwassim2 <wlahlali34@gmail.com>
2023-02-01 21:16:08 +0100

    ajouter le corp de composant ChatBody

--- client/src/components/ChatBody.jsx
+++ client/src/components/ChatBody.jsx
@@ -2,8 +2,22 @@ import React from 'react'
 
 function ChatBody() {
   return (
-    <div>ChatBody</div>
-  )
+<div className="flex flex-col gap-4">
+{/* client message */}
+<div>
+    <pre>
+        <span>Hy Chat GPT, Can YOU HELP ME</span>
+    </pre>
+</div>
+
+
+{/* ai message */}
+<div>
+    <pre>
+        <span>Yeah , I can help you with anything</span>
+    </pre>
+</div>
+</div>  )
 }
 
 export default ChatBody
