59144e3f3013bd8b4d2c512e5cc2820d7b543512
lahlaliwassim2 <wlahlali34@gmail.com>
2023-02-02 11:54:55 +0100

    Configuration de OpenAi en ajoutant la l organisation et API_Key

--- server/index.js
+++ server/index.js
@@ -2,13 +2,18 @@ import express from "express"
 import cors from 'cors'
 import bodyParser from "body-parser"
 import env from "dotenv"
-import {Configuration , OpenAIApi} from "openai"
+import {Configuration, Configuration , OpenAIApi} from "openai"
 
 const app = express()
 env.config()
 
 app.use(cors())
 app.use(bodyParser.json())
+
+const configuration = new Configuration({
+    organization : "org-6KrHi3KAC6B9t5q7q5v8eRcb",
+    apiKey : process.env.API_KEY
+})
 
 app.listen("3080", ()=>{
     console.log("port 3080")
