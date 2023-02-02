0d114832653317bf598eb139934da0d0628e9cca
lahlaliwassim2 <wlahlali34@gmail.com>
2023-02-02 14:00:52 +0100

    ajouter la methode sendMessage qui stock la valeur de l'inpute dansle state chat

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,8 +3,10 @@ import ChatBody from './components/ChatBody'
 import ChatInput from './components/ChatInput'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [chat, setChat] = useState([])
+  const sendMessage = async ()=>{
+    await Promise.resolve(setChat((prev)=>[...prev,message]))
+  }
   return (
     <div className='bg-[#1A232E] h-screen py-8  sm:px-28 relative text-white  overflow-hidden flex px-10 flex-col justify-between align-middle'>
       {/* header  */}
@@ -14,12 +16,12 @@ function App() {
 
        {/* body  */}
       <div className="h-[90%] overflow-auto w-full max-w-4xl min-w-[20rem] py-8 self-center">
-        <ChatBody />
+        <ChatBody chat={chat} />
       </div>
 
        {/* input  */}
        <div className="w-full max-w-4xl min-w-[20rem] self-center">
-        <ChatInput />
+        <ChatInput sendMessage={sendMessage} />
        </div>
     </div>
   )
