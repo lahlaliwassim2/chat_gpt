ecbba86d1e2f8dfdf9aa7daa918f590216207cd9
lahlaliwassim2 <wlahlali34@gmail.com>
2023-02-02 12:00:19 +0100

    ajouter le port pour les message et les parrametres de reponses

--- server/index.js
+++ server/index.js
@@ -2,18 +2,20 @@ import express from "express"
 import cors from 'cors'
 import bodyParser from "body-parser"
 import env from "dotenv"
-import {Configuration, Configuration , OpenAIApi} from "openai"
+import {Configuration , OpenAIApi} from "openai"
 
 const app = express()
 env.config()
 
 app.use(cors())
 app.use(bodyParser.json())
-
+//Configuration d OpenAi
 const configuration = new Configuration({
     organization : "org-6KrHi3KAC6B9t5q7q5v8eRcb",
     apiKey : process.env.API_KEY
 })
+
+const openai = new OpenAIApi(configuration)
 
 app.listen("3080", ()=>{
     console.log("port 3080")
@@ -23,4 +25,17 @@ app.get("/",  (req,res)=>{
     res.send("Hello world")
 })
 
-app.post
+app.post('/', async(req,res)=>{
+    const {message} =req.body
+    try {
+        const response = await openai.createCompletion({
+            model: "text-devinci-003",
+            prompt : `${message}`,
+            max_tokens: 100,
+            temperature: .5
+        })
+    } catch (error) {
+        console.log(error)
+        res.send(error).status(400)
+    }
+})
