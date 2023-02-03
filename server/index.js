bdc0589b13aa9bba7549de18e1831404cbf33b9f
lahlaliwassim2 <wlahlali34@gmail.com>
2023-02-03 11:05:56 +0100

    finaliser le projet

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json())
 
 // Configure open api
 const configuration = new Configuration({
-    organization: "org-6KrHi3KAC6B9t5q7q5v8eRcb",
+    organization: "PASTE YOUR ORGANIZATION ID HERE",
     apiKey: process.env.API_KEY // VISIT .env AND MAKE CHANGES
 })
 const openai = new OpenAIApi(configuration)
