c316afe3e4acaa63a4e792c2cf85c809902b6e82
lahlaliwassim2 <wlahlali34@gmail.com>
2023-02-03 09:43:31 +0100

    integration d api

--- server/index.js
+++ server/index.js
@@ -1,45 +1,50 @@
-import express from "express"
+import express from 'express'
 import cors from 'cors'
-import bodyParser from "body-parser"
-import env from "dotenv"
-import {Configuration , OpenAIApi} from "openai"
+import bodyParser from 'body-parser'
+import env from 'dotenv'
+import {Configuration, OpenAIApi} from 'openai'
 
 const app = express()
+
 env.config()
 
 app.use(cors())
 app.use(bodyParser.json())
-//Configuration d OpenAi
-const configuration = new Configuration({
-    organization : "org-6KrHi3KAC6B9t5q7q5v8eRcb",
-    apiKey : process.env.API_KEY
-})
 
+
+// Configure open api
+const configuration = new Configuration({
+    organization: "org-6KrHi3KAC6B9t5q7q5v8eRcb",
+    apiKey: process.env.API_KEY // VISIT .env AND MAKE CHANGES
+})
 const openai = new OpenAIApi(configuration)
 
-const PORT = process.env.PORT
-app.listen(PORT, ()=>{
-    console.log(`project run in port : ${PORT}`)
+
+// listeninng
+app.listen("3080", ()=>console.log("listening on port 3080"))
+
+
+// dummy route to test
+app.get("/", (req, res) => {
+    res.send("Hello World!")
 })
 
-app.get("/",  (req,res)=>{
-    res.send("Hello world")
-})
 
-app.post('/', async(req,res)=>{
-    const {message} =req.body
-    try {
+//post route for making requests
+app.post('/', async (req, res)=>{
+    const {message} = req.body
+
+    try{
         const response = await openai.createCompletion({
-            model: "text-devinci-003",
-            prompt : `${message}`,
+            model: "text-davinci-003",
+            prompt: `${message}`,
             max_tokens: 100,
             temperature: .5
         })
-        res.json({
-            message : response.data.choices[0].value
-        })
-    } catch (error) {
-        console.log(error)
-        res.send(error).status(400)
+        res.json({message: response.data.choices[0].text})
+
+    }catch(e){
+        console.log(e)
+        res.send(e).status(400)
     }
 })
